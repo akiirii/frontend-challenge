ded597416355ec10a6f70b7545f2bccd4faad9a7
akiirii <akiirii@gmail.com>
2018-12-04 22:15:22 +0100

    app and task init

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -1,11 +1,27 @@
 <template>
   <div id="app">
-    Fliit application
+    <manage-form v-bind:fields="config"></manage-form>
   </div>
 </template>
 
 <script>
+import Vue from 'vue';
+import vSelect from 'vue-select';
+import { Datetime } from 'vue-datetime';
+import config from './config.json';
+import Form from './components/task/task.vue';
+//import 'vue-datetime/dist/vue-datetime.css'
+
+Vue.component('v-select', vSelect);
+Vue.component('datetime', Datetime);
+
 export default {
-  name: 'app'
-}
+  name: 'app',
+  components: {
+    manageForm: Form
+  },
+  data: () => ({
+    config
+  }),
+};
 </script>
